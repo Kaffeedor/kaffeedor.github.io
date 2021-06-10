b89627f9b5c1b560ecdd55d04a8afc70c0550b9f
Kaffeedor <49571205+Kaffeedor@users.noreply.github.com>
2021-06-10 16:44:10 +0200

    aa

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-/* var button = document.getElementById("clickme"), count = 0;
-button.onclick = function() {
-    count += 1;
-    console.log(count);
-    button.innerHTML = "Clicks: " + count;
-}; */
+var score=0;
+
+function clicked() {
+    console.log("Clicked");
+    score++;
+    document.getElementById("scoreSpan").innerHTML = score;
+}
