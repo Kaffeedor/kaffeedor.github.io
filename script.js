eefd071a4ca441ea7d33bd237a2c50ea089f495a
Kaffeedor <49571205+Kaffeedor@users.noreply.github.com>
2021-06-10 08:45:00 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("clickme"), count = 0;
-        button.onclick = function() {
-            count += 1;
-            console.log(count)
-            button.innerHTML = "Clicks: " + count;
-        };
+button.onclick = function() {
+    count += 1;
+    console.log(count);
+    button.innerHTML = "Clicks: " + count;
+};
