5a76eb681bfddf9a3a7ae87a19fcc5644125bf1d
Fedor <49571205+Kaffeedor@users.noreply.github.com>
2021-11-22 09:25:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ function clicked() {
     score++;
     document.getElementById("scoreSpan").innerHTML = score;
 }
-var scoreL=0;
-function clickedL() {
-    console.log("Clicked");
-    score++;
-    document.getElementById("scoreSpanL").innerHTML = score;
-}
